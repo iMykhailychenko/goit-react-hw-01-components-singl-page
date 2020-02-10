852593cd803d2158bbcb2bc73a0fd5d635dd0054
Ihor Mykhailychenko <igor.c.m@ukr.net>
2020-02-10 23:25:09 +0200

    Updates

---
+++ precache-manifest.d2c1fbd0887a60fc2bfd9549baf4759c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49233450524882579aa791d3f297f3f3",
+    "url": "/goit-react-hw-01-components-singl-page/index.html"
+  },
+  {
+    "revision": "dd2256c1f8bbc03058ce",
+    "url": "/goit-react-hw-01-components-singl-page/static/css/main.08bafa60.chunk.css"
+  },
+  {
+    "revision": "cf64da9ec4be14c9f6eb",
+    "url": "/goit-react-hw-01-components-singl-page/static/js/2.5283aa73.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components-singl-page/static/js/2.5283aa73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2256c1f8bbc03058ce",
+    "url": "/goit-react-hw-01-components-singl-page/static/js/main.1cffb588.chunk.js"
+  },
+  {
+    "revision": "e31e4abfee4c3cfde056",
+    "url": "/goit-react-hw-01-components-singl-page/static/js/runtime-main.63cdab27.js"
+  }
+]);
