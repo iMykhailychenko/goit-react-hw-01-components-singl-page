f019ab3a1f8426a86a758815f05c24e53617d218
Ihor Mykhailychenko <igor.c.m@ukr.net>
2020-02-11 23:07:26 +0200

    done

--- src/components/social-profile/Profile.jsx
+++ src/components/social-profile/Profile.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-const Profile = ({ user }) => {
-  const stats = Object.entries(user.stats);
+const Profile = ({user}) => {
+  const {stats, avatar, name, tag, location} = user;
+  const statistic = Object.entries(stats);
+
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
-        <img src={user.avatar} alt="user avatar" className={styles.avatar} />
-        <p className={styles.name}>{user.name}</p>
-        <p className={styles.tag}>{`@${user.tag}`}</p>
-        <p className={styles.location}>{user.location}</p>
+        <img src={avatar} alt="user avatar" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{`@${tag}`}</p>
+        <p className={styles.location}>{location}</p>
       </div>
 
       <ul className={styles.stats}>
-        {stats.map((item, index) => (
+        {statistic.map((item, index) => (
           <li key={index} className={styles.st_items}>
             <span className={styles.label}>{item[0]}</span>
             <span className={styles.quantity}>{item[1]}</span>
@@ -31,7 +33,7 @@ Profile.propTypes = {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: PropTypes.objectOf(PropTypes.number),
+    stats: PropTypes.objectOf(PropTypes.number.isRequired),
   }),
 };
 
